81183abfbf38b7485666ce16119b3b033dbcfa7f
JonnClaude <jonnclaudehoffman@gmail.com>
2021-03-13 13:53:13 +0200

    Change Age
    Changed the age to align with JC
    Changed getPersonOfAge method to be cleaner

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,22 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int age = 30;
+        int age = 29;
 
         printToCommandLine(getPersonOfAge(age));
     }
 
     private static String getPersonOfAge(int age){
-        String person;
-        switch (age) {
-            case 30:
-                person = "gideon";
-            case 29:
-                person = "jc";
-            default:
-                person = "no such cunt available";
-        }
-        return person;
+        return switch (age) {
+            case 30 -> "gideon";
+            case 29 -> "jc";
+            default -> "no such cunt available";
+        };
     }
 
     private static void printToCommandLine(String stringToPrint) {
